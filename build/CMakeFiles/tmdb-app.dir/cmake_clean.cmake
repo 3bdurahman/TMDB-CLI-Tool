db138175d75e1950dbc21a987f0c261ec76563f3
3bdurahman <3bdo.i.ha4em@gmail.com>
2025-03-16 02:51:48 +0200

    tmdb-app

---
+++ build/CMakeFiles/tmdb-app.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tmdb-app.dir/source/TMDB.cpp.obj"
+  "CMakeFiles/tmdb-app.dir/source/TMDB.cpp.obj.d"
+  "CMakeFiles/tmdb-app.dir/source/tmdb-app.cpp.obj"
+  "CMakeFiles/tmdb-app.dir/source/tmdb-app.cpp.obj.d"
+  "libtmdb-app.dll.a"
+  "tmdb-app.exe"
+  "tmdb-app.exe.manifest"
+  "tmdb-app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tmdb-app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
